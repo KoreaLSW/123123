f3cb848ac1820369cc7c58864b452f316bd9a554
KoreaLSW <89916970+KoreaLSW@users.noreply.github.com>
2022-11-24 21:22:06 +0900

    추가

---
+++ src/components/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TailwindComponent() {
+    return (
+        <div>
+            <h1 className='text-5xl'>안녕11</h1>
+            <button className='bg-blue-500'>Button</button>
+        </div>
+    );
+}
