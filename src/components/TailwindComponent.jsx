b55033a752e7651ace5bdb7fe20a2ada455e09ce
KoreaLSW <89916970+KoreaLSW@users.noreply.github.com>
2022-11-25 13:48:12 +0900

    first commit

--- src/components/TailwindComponent.jsx
+++ src/components/TailwindComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function TailwindComponent() {
     return (
         <div>
-            <h1 className='text-5xl'>안녕11232312311</h1>
+            <h1 className='text-5xl'>좀 되라 ㅡㅡ</h1>
             <button className='bg-blue-500'>Button</button>
         </div>
     );
